45413940bc5a4af2c0a5447e51b75842ec96da1f
justisabelll <roywashington1547@gmail.com>
2025-07-05 15:34:35 -0500

    remove boilerplate

---
+++ frontend/app/views/hello-world.tsx
@@ -0,0 +1,8 @@
+export default function HelloWorld() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">Hello, World!</h1>
+      <p className="text-lg">Welcome to your new React app.</p>
+    </div>
+  );
+}
